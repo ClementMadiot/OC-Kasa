36b9acb6a7c2b05be063763978bb7c9ac6e29777
ClementMadiot <111243053+ClementMadiot@users.noreply.github.com>
2023-08-11 18:52:18 +0200

    Update slider.jsx

--- src/components/Slider/slider.jsx
+++ src/components/Slider/slider.jsx
@@ -6,26 +6,26 @@ import '../../pages/Loft/loft.scss'
 
 
 function Slider({ img }) {
-
   const [current, setCurrent] = useState(0);
   const length = img.length;
 
   const nextSlide = () => {
     setCurrent(current === length - 1 ? 0 : current + 1)
   }
-  const pervSlide = () => {
+  const prevSlide = () => {
     setCurrent(current === 0 ? length - 1 : current - 1)
   }
+
   return (
     <section className='Caroussel-Container'>
       <button
       type='button'
-      className='prev-button'
-      onClick={pervSlide}
+      className={`${length === 1 ? 'no-button' : 'prev-button'}`}
+      onClick={prevSlide}
       ><FontAwesomeIcon icon="fa-solid fa-chevron-left"/></button>
       <button
       type='button'
-      className='next-button'
+      className={`${length === 1 ? 'no-button' : 'next-button'}`}
       onClick={nextSlide}
       ><FontAwesomeIcon icon="fa-solid fa-chevron-right"/></button>
       
@@ -34,7 +34,7 @@ function Slider({ img }) {
           {index === current && <img className='Slide-Img' src={picture} alt={`${index}`} />}
         </div>
       ))}
-        
+      <div className='indicator'>{current + 1}/{length}</div>
     </section>
       
 
